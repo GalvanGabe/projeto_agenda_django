dabfc8a7ca98566a9fc8eb434336eb9b63cfd7a5
Gabriel Galvan <galvan.gabriel@hotmail.com>
2025-04-11 23:16:30 -0300

    creating users views

--- contact/admin.py
+++ contact/admin.py
@@ -4,3 +4,10 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'phone', 'email', 'created_date')
+    ordering = '-id',
+    search_fields = ('id', 'first_name', 'last_name'),
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
