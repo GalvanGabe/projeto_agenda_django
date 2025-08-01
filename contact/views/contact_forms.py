57458d1682fb8f6451b237c5cee7f8d968afa5f4
Gabriel Galvan <galvan.gabriel@hotmail.com>
2025-07-31 21:49:14 -0300

    authenticate system

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,11 @@
-from typing import Any, Dict
-
-from django import forms
-from django.core.exceptions import ValidationError
-from django.core.paginator import Paginator
-from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
 
 from contact.forms import ContactForm
 from contact.models import Contact
     
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -42,6 +38,7 @@ def create(request):
         context
     )
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id)
     form_action = reverse('contact:update', args=(contact_id))
@@ -75,6 +72,7 @@ def update(request, contact_id):
         context
     )
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
         Contact, pk=contact_id, show=True
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,4 +1,5 @@
 from django.contrib import auth, messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 
@@ -45,6 +46,7 @@ def login_view(request):
         }
     )
 
+@login_required(login_url='contact:login')
 def user_update(request):
     form = RegisterUpdateForm(instance=request.user)
 
@@ -72,6 +74,7 @@ def user_update(request):
 
     return redirect('contact:user_update')
 
+@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
